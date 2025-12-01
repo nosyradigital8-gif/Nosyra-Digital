228759edd886cc2361ccc8dbe4be391481467f5f
nosyradigital8-gif <nosyradigital8@gmail.com>
2025-12-01 06:59:39 +0100

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -51,7 +51,7 @@ export function Header() {
                   priority
                 />
               </div>
-              <span className="text-2xl font-bold text-[#0a0e27] hidden sm:inline">Nosyra</span>
+              
             </Link>
 
             {/* Desktop Menu */}
